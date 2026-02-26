46d88133ef3f4bdad7c5e37743b8e2bc4a97dc36
Ananya-Iyer <iyerananyasesha@gmail.com>
2026-02-26 11:25:40 +0000

    bug fixes

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -61,17 +61,17 @@ input_data = pd.DataFrame([{
     'MonthlyIncome': monthly_income
 }])
 
-#------------------------For debugging----------------
+# #------------------------For debugging----------------
 
-expected = list(model.feature_names_in_)
-actual = list(input_data.columns)
+# expected = list(model.feature_names_in_)
+# actual = list(input_data.columns)
 
-st.write("Expected columns:", expected)
-st.write("Actual columns:", actual)
+# st.write("Expected columns:", expected)
+# st.write("Actual columns:", actual)
 
-st.write("Missing columns:", set(expected) - set(actual))
-st.write("Extra columns:", set(actual) - set(expected))
-#-----------------------------------------
+# st.write("Missing columns:", set(expected) - set(actual))
+# st.write("Extra columns:", set(actual) - set(expected))
+# #--------------------------------------------------------
 
 
 # Predict button
